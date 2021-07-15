a2d3eb943f9180a8842d48a8a8d21a9c417b328a
023577 <jing.timblique.a6g@ap.denso.com>
2021-07-16 07:35:17 +0800

    Revised folder structure + edited html file

--- assignment-script.js
+++
@@ -1,9 +0,0 @@
-//assignment 1: values and variables
-
-let country = "Philippines";
-let continent = "Asia";
-let population = "7 million";
-
-console.log(country);
-console.log(continent);
-console.log(population);

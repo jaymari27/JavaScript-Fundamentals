91ee2e697445ea40bc6c4af12b760e69a8447384
023577 <jing.timblique.a6g@ap.denso.com>
2021-07-15 15:19:35 +0800

    Added renamed script file

---
+++ assignment-script.js
@@ -0,0 +1,9 @@
+//assignment 1: values and variables
+
+let country = "Philippines";
+let continent = "Asia";
+let population = "7 million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
